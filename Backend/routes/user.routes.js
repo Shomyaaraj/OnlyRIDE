4383eb2cbe838dd670c97ea98bd0752596e1e871
Shomyaaraj <shomya725@gmail.com>
2026-02-18 02:30:28 +0530

    first commit

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router =express.Router();
+const {body}= require("express-validator")
+const userController = require('../controllers/user.Controller');
+
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('First name must be atleast 3 char long'),
+    body('password').isLength({min:6}).withMessage('Password must be atleast 6 digit long')
+],
+    userController.registerUser
+)
+
+
+
+module.exports=router;
