b351e04294e268844b9a44c5bb1b50fcf5f3a90b
Shomyaaraj <shomya725@gmail.com>
2026-03-07 01:17:17 +0530

    vehicle Panel

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
-
+const LocationSearchPanel = (props) => {
+console.log(props)
   //sample array for loactions
   const locations=[
     "Nikhar kunj, Shakti Market , Hanuman Nagar patna -20",
@@ -13,8 +13,11 @@ const LocationSearchPanel = () => {
     <div>
       {/*this is a sample data */}
       {
-        locations.map(function(elem){
-          return   <div className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
+        locations.map(function(elem, idx){
+          return   <div key={idx} onClick={()=>{
+            props.setVehiclePanel(true)
+            props.setPanelOpen(false)
+          }} className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
         <h2 className='bg-[#eee] h-8 w-12 rounded-full items-center justify-center'><i className="ri-map-pin-2-fill "></i></h2>
         <h4 className='font-medium'>{elem}</h4>
       </div>
