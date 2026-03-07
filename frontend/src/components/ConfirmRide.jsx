a4003547acfa635f58e74e6209cbce60f71407c2
Shomyaaraj <shomya725@gmail.com>
2026-03-08 01:02:34 +0530

    COnfirm Ride Page

--- frontend/src/components/ConfirmRide.jsx
+++ frontend/src/components/ConfirmRide.jsx
@@ -9,15 +9,31 @@ const ConfirmRide = (props) => {
          <h3 className='text-2xl font-semibold mb-5'>Confirm your Ride</h3>
         <div className='flex gap-2 justify-between flex-col items-center'>
             <img className='h-20' src="https://imgs.search.brave.com/wtpH-N99a8_OkMZ2p-swZAHAYVRzIqz00GPla9gMrYk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjkv/OTE0LzczMy9zbWFs/bC93aGl0ZS1jaXR5/LWNhci1pc29sYXRl/ZC1vbi10cmFuc3Bh/cmVudC1iYWNrZ3Jv/dW5kLTNkLXJlbmRl/cmluZy1pbGx1c3Ry/YXRpb24tZnJlZS1w/bmcucG5n" />
-            <div className='w-full'>
-                <div></div>
-                <div></div>
-                <dialog></dialog>
+            <div className='w-full mt-5'>
+                <div className='flex items-center gap-5 p-3 border-b-2 border-gray-200'>
+                  <i className="ri-user-location-fill"></i>
+                  <div>
+                    <h3 className='text-lg font-medium'>562/11-A</h3>
+                    <p className='text-sm -mt-1 text-gray-600'>panta-20, Bihar</p>
+                  </div>
+                </div>
+                <div className='flex items-center gap-5 p-3 border-b-2 border-gray-200'>
+                  <i className="text-lg ri-map-pin-2-fill"></i>
+                  <div>
+                    <h3 className='text-lg font-medium'>562/11-A</h3>
+                    <p className='text-sm -mt-1 text-gray-600'>panta-20, Bihar</p>
+                  </div>
+                </div>
+                <div className='flex items-center gap-5 p-3 '>
+                  <i className="ri-wallet-3-fill"></i>
+                  <div>
+                    <h3 className='text-lg font-medium'>$193.60</h3>
+                    <p className='text-sm -mt-1 text-gray-600'>panta-20, Bihar</p>
+                  </div>
+                </div>
             </div>
-            <div>
-                <button className='w-full bg-green-400 text-white font-semibold p-2 rounded-lg'>Confirm Ride</button>
-            </div>
-
+            
+                <button className='w-full bg-green-600 text-white font-semibold p-2 rounded-lg mt-5'>Confirm Ride</button>
         </div>
     </div>
   )
